fc075b5e9325576a8729372d5616310cc35b258e
Don Joseph Lachica <44009377+Zerotsuu@users.noreply.github.com>
2024-11-07 13:39:20 +0800

    FIxed redirect url after signing up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignOutUrl={"/"}
+    signUpForceRedirectUrl={"/"}
+    signInFallbackRedirectUrl={"/"}>
       <html lang="en">
         <body className={`font-sans ${inter.variable}`}>
           <TRPCReactProvider>
